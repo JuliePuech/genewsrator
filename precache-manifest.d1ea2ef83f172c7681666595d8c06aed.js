c71b0ddaa248793b20c0df692c5d2cd38464cb22
Lucas Sifoni <lucas@lucassifoni.info>
2018-07-02 11:24:34 +0200

    First build

---
+++ precache-manifest.d1ea2ef83f172c7681666595d8c06aed.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "038fbb3302d4de3fa436f3f7fb4e2b8c",
+    "url": "/genewsrator/img/loading.svg"
+  },
+  {
+    "revision": "26a17dee9f66f00c1c16",
+    "url": "/genewsrator/js/chunk-vendors.26a17dee.js"
+  },
+  {
+    "revision": "e392c6bc898ad198c98971380f443e4f",
+    "url": "/genewsrator/img/appstore.png"
+  },
+  {
+    "revision": "2955848e9d9201a15aa2",
+    "url": "/genewsrator/js/app.2955848e.js"
+  },
+  {
+    "revision": "b3317b01755c064bbdd7ab35a7cb05ec",
+    "url": "/genewsrator/index.html"
+  },
+  {
+    "revision": "c438f6bb9fd22dd820b5ba3f875e698f",
+    "url": "/genewsrator/img/loading.gif"
+  },
+  {
+    "revision": "42485fc377a6bd6e479db9cac6580135",
+    "url": "/genewsrator/img/home.png"
+  },
+  {
+    "revision": "7017743811975f3f3ce3574613f3bf26",
+    "url": "/genewsrator/img/googleplay.png"
+  },
+  {
+    "revision": "b2c819724cb9573d94660b07dd553059",
+    "url": "/genewsrator/img/croix.png"
+  },
+  {
+    "revision": "14ab56561ddcb809868feb38200fb8bc",
+    "url": "/genewsrator/img/button.png"
+  },
+  {
+    "revision": "edce5cec9f5981e68dde2a3645340396",
+    "url": "/genewsrator/img/banner.png"
+  },
+  {
+    "revision": "f22cbaf142c2f2426887f0250d8b5d87",
+    "url": "/genewsrator/img/avion.png"
+  },
+  {
+    "revision": "2955848e9d9201a15aa2",
+    "url": "/genewsrator/css/app.f5c597a6.css"
+  },
+  {
+    "revision": "cc93919346e0fa7226d95cb8182ba951",
+    "url": "/genewsrator/404.html"
+  }
+];
